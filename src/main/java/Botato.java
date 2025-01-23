141c42f0742ed278aa25cb6a6c315e755fc2b454
wenruu <zhaowenrui2002@gmail.com>
2025-01-24 00:33:40 +0800

    Level-1

--- src/main/java/Botato.java
+++ src/main/java/Botato.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Botato {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         String line = "-------------------------------------------------";
         String helloMessage = line + "\nHello from Botato!\nWhat can I do for you?\n" + line;
         String goodbyeMessage = line + "\nHope I helped! See you again!\n" + line;
         System.out.println(helloMessage);
-        System.out.println(goodbyeMessage);
+        while (true) {
+            String cmd = reader.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println(goodbyeMessage);
+                break;
+            }
+            System.out.println(line + "\n" + cmd + "\n" + line);
+        }
     }
 }
