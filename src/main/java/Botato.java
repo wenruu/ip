cdf1725334dd357dfa39b80af53079f3234dfca2
wenruu <zhaowenrui2002@gmail.com>
2025-01-24 00:22:24 +0800

    Level-0

--- src/main/java/Botato.java
+++ src/main/java/Botato.java
@@ -1,10 +1,9 @@
 public class Botato {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "-------------------------------------------------";
+        String helloMessage = line + "\nHello from Botato!\nWhat can I do for you?\n" + line;
+        String goodbyeMessage = line + "\nHope I helped! See you again!\n" + line;
+        System.out.println(helloMessage);
+        System.out.println(goodbyeMessage);
     }
 }
