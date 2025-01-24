8d09f0b8ed533214d02e99d61a9a5179495c94f3
wenruu <e1122418@u.nus.edu>
2025-01-24 10:32:17 +0800

    A-TextUiTesting

--- src/main/java/Botato.java
+++ src/main/java/Botato.java
@@ -16,40 +16,49 @@ public class BOTato {
                 System.out.println(goodbyeMessage);
                 terminate = 1;
             } else if (cmd.equals("list")) {
-                System.out.println(line + "\nHere are your current tasks: ");
+                System.out.println(line + "\nHere are your current tasks:");
                 IntStream.range(0, tasks.size())
                         .forEach(i -> System.out.println((i + 1) + "." + tasks.get(i)));
                 System.out.println(line);
             } else if (cmd.startsWith("mark")) {
                 if (cmd.length() == 6 && Character.isDigit(cmd.charAt(5))) {
-                    int taskNum = Character.getNumericValue(cmd.charAt(5)) - 1;
+                    int taskNum = Character.getNumericValue(cmd.charAt(5));
                     if (taskNum > tasks.size()) {
-                        System.out.println(line + "\nYou don't have that many tasks!\n" + line);
+                        System.out.println(line + (tasks.isEmpty() ? "\nYou have no tasks!\n" :
+                                "\nYou only have " + tasks.size() + " task(s)!\n") + line);
+                        continue;
+                    } else if (taskNum <= 0){
+                        System.out.println(line + "\nInvalid Task Number!\n" + line);
                         continue;
                     }
-                    tasks.get(taskNum).isDone = true;
+                    tasks.get(taskNum - 1).isDone = true;
                     System.out.println(line + "\nGood job! I've marked this task as done:\n"
-                            + tasks.get(taskNum) + "\n" + line);
+                            + tasks.get(taskNum - 1) + "\n" + line);
                 }
             } else if (cmd.startsWith("unmark")) {
                 if (cmd.length() == 8 && Character.isDigit(cmd.charAt(7))) {
-                    int taskNum = Character.getNumericValue(cmd.charAt(7)) - 1;
+                    int taskNum = Character.getNumericValue(cmd.charAt(7));
                     if (taskNum > tasks.size()) {
-                        System.out.println(line + "\nYou don't have that many tasks!\n" + line);
+                        System.out.println(line + (tasks.isEmpty() ? "\nYou have no tasks!\n" :
+                                "\nYou only have " + tasks.size() + " task(s)!\n") + line);
+                        continue;
+                    } else if (taskNum <= 0){
+                        System.out.println(line + "\nInvalid Task Number!\n" + line);
                         continue;
                     }
-                    tasks.get(taskNum).isDone = false;
-                    System.out.println( line + "\nAww... guess you didn't do this task after all:\n"
-                            + tasks.get(taskNum) + "\n" + line);
+                    tasks.get(taskNum - 1).isDone = false;
+                    System.out.println(line + "\nAww... guess you didn't do this task after all:\n"
+                            + tasks.get(taskNum - 1) + "\n" + line);
                 }
-            } else if (cmd.startsWith("todo")){
-                tasks.add(new Task(cmd));
-                System.out.println(line + "\nadded: " + cmd + "\n" + line);
+            } else if (cmd.startsWith("todo ")){
+                Task task = new Task(cmd.substring(5));
+                tasks.add(task);
+                System.out.println(line + "\nAdded:\n" + task + "\n" + line);
             } else if (cmd.startsWith("deadline")){
                 if (cmd.contains("/by")) {
                     Task task = new Deadline(cmd);
                     tasks.add(task);
-                    System.out.println(line + "\nadded: " + task + "\n" + line);
+                    System.out.println(line + "\nAdded:\n" + task + "\n" + line);
                 } else {
                     System.out.println(line + "\nMake sure your 'deadline' command contains '/by' to indicate your " +
                             "deadline!\n" + line);
@@ -58,7 +67,7 @@ public class BOTato {
                 if (cmd.contains("/to") && cmd.contains(("/from"))) {
                     Task task = new Event(cmd);
                     tasks.add(task);
-                    System.out.println(line + "\nadded: " + task + "\n" + line);
+                    System.out.println(line + "\nAdded:\n" + task + "\n" + line);
                 } else {
                     System.out.println(line + "\nMake sure your 'deadline' command contains '/from' and '/to' to set " +
                             "your deadline!\n" + line);
