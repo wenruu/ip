9ffe6b77698577b239b9be6fd88999499c34cfe5
wenruu <e1122418@u.nus.edu>
2025-02-01 16:06:29 +0800

    branch-Level-8

--- src/main/java/Botato.java
+++ src/main/java/Botato.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class BOTato {
+public class Botato {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String line = "---------------------------------------------------------------------------------------------------------------";
@@ -17,22 +17,26 @@ public class BOTato {
             // Keeps reading commands until "bye" is typed
             String cmd = reader.nextLine();
             if (cmd.equals("bye")) {
+                // Save data and exit chatbot
                 System.out.println(line);
                 writeData(tasks);
-                System.out.println("\nHope I helped! See you again!\n" + line);
+                System.out.println("Hope I helped! See you again!\n" + line);
                 terminate = 1;
             } else if (cmd.equals("list")) {
+                // Display list of tasks
                 System.out.println(line + "\nHere are your current tasks:");
                 IntStream.range(0, tasks.size())
                         .forEach(i -> System.out.println((i + 1) + "." + tasks.get(i)));
                 System.out.println(line);
             } else if (cmd.startsWith("mark ")) {
+                // Mark specific task as completed
                 if (cmd.substring(4).isBlank()) {
                     System.out.println(line + "\nPlease choose the task you completed! You have " + tasks.size() +
                             " tasks!\n" + line);
                 }
                 String taskStr = cmd.substring(4).strip(); // extract desired task index
-                if (taskStr.matches("-?\\d+")) { // validate that taskStr is an integer
+                if (taskStr.matches("-?\\d+")) {
+                    // Validate that taskStr is an integer
                     int taskNum = Integer.parseInt(taskStr);
                     if (taskNum > tasks.size()) {
                         System.out.println(line + (tasks.isEmpty() ? "\nYou have no tasks!\n" :
@@ -50,12 +54,14 @@ public class BOTato {
                             " to mark as done!\n" + line);
                 }
             } else if (cmd.startsWith("unmark ")) {
+                // Mark specified task as not done
                 if (cmd.substring(6).isBlank()) {
                     System.out.println(line + "\nPlease choose the task you completed! You have " + tasks.size() +
                             " task(s)!\n" + line);
                 }
                 String taskStr = cmd.substring(6).strip(); // extract desired task index
-                if (taskStr.matches("-?\\d+")) { // validate that taskStr is an integer
+                if (taskStr.matches("-?\\d+")) {
+                    // Validate that taskStr is an integer
                     int taskNum = Integer.parseInt(taskStr);
                     if (taskNum > tasks.size()) {
                         System.out.println(line + (tasks.isEmpty() ? "\nYou have no tasks!\n" :
@@ -68,26 +74,30 @@ public class BOTato {
                     tasks.get(taskNum - 1).isDone = false;
                     System.out.println(line + "\nAww... Guess you didn't do this after all:\n"
                             + tasks.get(taskNum - 1) + "\n" + line);
-                } else { // if taskStr is not an integer
+                } else {
+                    // Handle invalid cases
                     System.out.println(line + "\nPlease choose a value between 1 and " + tasks.size() +
                             " to mark as not done!\n" + line);
                 }
             } else if (cmd.startsWith("todo ")){
-                Task task = new Task(cmd);
-                if (task.msg.isEmpty()) {
+                // Add a Task of type Todo to tasks
+                Task task = new Todo(cmd);
+                if (task.description.isEmpty()) {
                     System.out.println(line + "\nPlease add a description for your task!\n" + line);
                     continue;
                 }
                 tasks.add(task);
                 System.out.println(line + "\nAdded:\n" + task + "\n" + line);
             } else if (cmd.startsWith("deadline")){
+                // Add a Task of type Deadline to tasks
                 if (cmd.contains("/by")) {
                     Deadline task = new Deadline(cmd);
-                    if (task.msg.isEmpty()) {
+                    if (task.description.isEmpty()) {
                         System.out.println(line + "\nPlease add a description to your task!\n" + line);
                         continue;
-                    } else if (task.by.isEmpty()) {
-                        System.out.println(line + "\nPlease add a deadline!\n" + line);
+                    } else if (task.by == null) {
+                        System.out.println(line + "\nPlease add a deadline in a valid format!\n"
+                                + "Type 'dateformats' to get a full list of supported formats.\n" + line);
                         continue;
                     }
                     tasks.add(task);
@@ -97,38 +107,47 @@ public class BOTato {
                             "deadline!\n" + line);
                 }
             } else if (cmd.startsWith("event ")) {
+                // Add a Task of type Event to tasks
                 if (cmd.contains("/to") && cmd.contains(("/from"))) {
                     Event task = new Event(cmd);
-                    if (task.msg.isEmpty()) {
+                    if (task.description.isEmpty()) {
                         System.out.println(line + "\nPlease add a description to your task!\n" + line);
                         continue;
-                    } else if (task.from.isEmpty()) {
-                        System.out.println(line + "\nPlease add a starting date!\n" + line);
+                    } else if (task.from == null) {
+                        System.out.println(line + "\nPlease add a starting date in a valid format!\n"
+                                + "Type 'dateformats' to get a full list of supported formats.\n" + line);
                         continue;
-                    } else if (task.to.isEmpty()) {
-                        System.out.println(line + "\nPlease add an ending date!\n" + line);
+                    } else if (task.to == null) {
+                        System.out.println(line + "\nPlease add an ending date in a valid format!\n"
+                                + "Type 'dateformats' to get a full list of supported formats.\n" + line);
                         continue;
                     }
                     tasks.add(task);
                     System.out.println(line + "\nAdded:\n" + task + "\n" + line);
                 } else {
-                    System.out.println(line + "\nMake sure your 'event' command contains '/from' and '/to' to set " +
-                            "your start and end dates!\n" + line);
+                    System.out.println(line + "\nMake sure your 'event' command contains '/from' and '/to' to set " 
+                            + "your start and end dates!\n" + line);
                 }
             } else if (cmd.startsWith("delete ")) {
-                if (cmd.substring(6).strip().isBlank()) { // no task number chosen
-                    System.out.println(line + "\nPlease choose a task you want to delete! You have " + tasks.size() +
-                            " tasks.\n" + line);
+                // Delete a specified task based on task number given
+                if (cmd.substring(6).strip().isBlank()) {
+                    // Handle no task number given
+                    System.out.println(line + "\nPlease choose a task you want to delete! You have " + tasks.size() 
+                            + " tasks.\n" + line);
                     continue;
                 }
-                String taskStr = cmd.substring(6).strip(); // extract desired task index
-                if (taskStr.matches("-?\\d+")) { // validate that taskStr is an integer
-                    int taskNum = Integer.parseInt(taskStr); // convert to int
+                String taskStr = cmd.substring(6).strip();
+                // Extract desired task number
+                if (taskStr.matches("-?\\d+")) {
+                    // Validate that taskStr is an integer
+                    int taskNum = Integer.parseInt(taskStr);
+                    // Convert to int
                     if (taskNum > tasks.size()) {
-                        System.out.println(line + (tasks.isEmpty() ? "\nYou have no tasks!\n" :
-                                "\nYou only have " + tasks.size() + " task(s)!\n") + line);
+                        System.out.println(line + (tasks.isEmpty() ? "\nYou have no tasks!\n" 
+                                : "\nYou only have " + tasks.size() + " task(s)!\n") + line);
                         continue;
                     } else if (taskNum <= 0) {
+                        // Handle invalid task numbers
                         System.out.println(line + "\nTask number cannot be less than 1!\n" + line);
                         continue;
                     }
@@ -136,12 +155,28 @@ public class BOTato {
                     tasks.remove(taskNum - 1);
                     System.out.println((line + "\nTask has been successfully removed:\n" + temp + "\n" + line));
                 } else {
-                    System.out.println(line + "\nPlease choose a value between 1 and " + tasks.size() +
-                            " to delete!\n" + line);
+                    System.out.println(line + "\nPlease choose a value between 1 and " + tasks.size() 
+                            + " to delete!\n" + line);
                 }
+            } else if (cmd.equals("dateformats")) {
+                System.out.println(line + "\nSupported date formats include:\n" +
+                        "yyyy-MM-dd HH:mm:ss\n" +
+                        "yyyy-MM-dd HH:mm\n" +
+                        "yyyy-MM-dd\n" +
+                        "dd/MM/yyyy HH:mm:ss\n" +
+                        "dd/MM/yyyy HH:mm\n" +
+                        "dd/MM/yyyy\n" +
+                        "dd/MM/yy HH:mm:ss\n" +
+                        "dd/MM/yy HH:mm\n" +
+                        "dd/MM/yy\n" +
+                        "dd MMM yyyy HH:mm:ss\n" +
+                        "dd MMM yyyy HH:mm\n" +
+                        "dd MMM yyyy\n" + line);
             } else {
-                System.out.println(line + "\nSorry, I don't know what that means... Here are the commands you can " +
-                        "use:\n'bye', 'list', 'mark ', 'unmark ', 'todo ', 'deadline ', 'event ', 'delete '\n" + line);
+                // Handle unknown commands
+                System.out.println(line + "\nSorry, I don't know what that means... Here are the commands you can use:" 
+                        + "\n'bye', 'list', 'mark ', 'unmark ', 'todo ', 'deadline ', 'event ', 'delete ', "
+                        + "dateformats\n" + line);
             }
         }
     }
@@ -168,7 +203,7 @@ public class BOTato {
     }
 
     /**
-     * Loads data from './data/data/txt'.
+     * Loads data from './data/data.txt'.
      * If file does not exist, return empty ArrayList instead.
      * @return ArrayList containing tasks.
      */
@@ -187,7 +222,6 @@ public class BOTato {
                 return new ArrayList<>();
             }
         } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
             return new ArrayList<>();
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,94 @@
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.Temporal;
 
+/**
+ * Represents a task with a description and a completion status.
+ * This class is serializable, allowing instances to be serialized and saved to storage.
+ */
 public class Task implements Serializable {
-    protected String description;
     protected boolean isDone;
-    final String msg;
+    protected String description;
 
-    public Task(String cmd) {
-        this.description = cmd;
-        this.isDone = false;
-        this.msg = cmd.substring(4).strip();
-    }
-
+    /**
+     * Checks task completion and returns corresponding String status icon
+     * @return "X" if Task has been marked as completed, " " otherwise
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    @Override
-    public String toString() {
-            return String.format("[%s] Todo: %s", getStatusIcon(), msg);
+    /**
+     * Parses a given date string into a {@link LocalDateTime} object based on various supported date formats.
+     * If the date string does not include a time component, the provided {@link LocalTime} defaultTime is used
+     * to construct the {@link LocalDateTime} object.
+     * <p>
+     * Supported date formats include:
+     * - "yyyy-MM-dd HH:mm:ss"
+     * - "yyyy-MM-dd HH:mm"
+     * - "yyyy-MM-dd"
+     * - "dd/MM/yyyy HH:mm:ss"
+     * - "dd/MM/yyyy HH:mm"
+     * - "dd/MM/yyyy"
+     * - "dd/MM/yy HH:mm:ss"
+     * - "dd/MM/yy HH:mm"
+     * - "dd/MM/yy"
+     * - "dd MMM yyyy HH:mm:ss"
+     * - "dd MMM yyyy HH:mm"
+     * - "dd MMM yyyy"
+     *</p>
+     * @param dateStr     The date string to parse.
+     * @param defaultTime The default time to use if the date string does not include a time component.
+     * @return A {@link LocalDateTime} object representing the parsed date and time or null if the format is
+     * unsupported.
+     */
+    public LocalDateTime parseDate(String dateStr, LocalTime defaultTime) {
+        try {
+            if (dateStr.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            }
+            if (dateStr.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+            }
+            if (dateStr.matches("\\d{4}-\\d{2}-\\d{2}")) {
+                LocalDate date = LocalDate.parse(dateStr);
+                return LocalDateTime.of(date, defaultTime);
+            }
+            if (dateStr.matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+            }
+            if (dateStr.matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+            }
+            if (dateStr.matches("\\d{2}/\\d{2}/\\d{4}")) {
+                LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                return LocalDateTime.of(date, defaultTime);
+            }
+            if (dateStr.matches("\\d{2}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss"));
+            }
+            if (dateStr.matches("\\d{2}/\\d{2}/\\d{2} \\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yy HH:mm"));
+            }
+            if (dateStr.matches("\\d{2}/\\d{2}/\\d{2}")) {
+                LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yy"));
+                return LocalDateTime.of(date, defaultTime);
+            }
+            if (dateStr.matches("\\d{2} \\w{3} \\d{4} \\d{2}:\\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss"));
+            }
+            if (dateStr.matches("\\d{2} \\w{3} \\d{4} \\d{2}:\\d{2}")) {
+                return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm"));
+            }
+            if (dateStr.matches("\\d{2} \\w{3} \\d{4}")) {
+                LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("dd MMM yyyy"));
+                return LocalDateTime.of(date, defaultTime);
+            }
+        } catch (DateTimeParseException ignored) {}
+        return null;
     }
 }
