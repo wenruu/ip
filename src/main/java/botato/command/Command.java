2c8cde71f967831991e5b66891e626311ece32d6
wenruu <zhaowenrui2002@gmail.com>
2025-02-09 11:28:31 +0800

    Add JavaDocs to more classes

--- src/main/java/botato/command/Command.java
+++ src/main/java/botato/command/Command.java
@@ -4,14 +4,28 @@ import botato.Storage;
 import botato.TaskList;
 import botato.Ui;
 
+/**
+ * The Command class represents an abstract command that can be executed by the bot.
+ * It defines the common behavior for all specific commands in the bot application.
+ * Each command has an execution logic and can indicate whether it causes the bot to exit.
+ */
 public abstract class Command {
     protected boolean isExit = false;
 
+    /**
+     * Executes the specific logic of the command. This method must be implemented by subclasses
+     * to define the behavior for each command.
+     *
+     * @param tasks The task list that may be modified during command execution.
+     * @param ui The user interface that may be updated based on the command execution.
+     */
     public abstract void execute(TaskList tasks, Ui ui);
 
     /**
-     * isExit is only true if 'bye' command was typed.
-     * @return isExit
+     * Returns whether the command should terminate the bot application.
+     * The exit condition is only true if the "bye" command was typed.
+     *
+     * @return boolean indicating if the command causes the bot to exit.
      */
     public boolean isExit() {
         return isExit;
